6d706ddbcdeddfc3071820baac7066a43ac76d63
Eric Moore <EricGittaMoore@duck.com>
2025-03-11 23:13:47 +0800

    feat: 更新发布工作流和调试配置，优化构建过程

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,6 +1,5 @@
 import gulp from "gulp";
 import { deleteAsync } from "del";
-import path from "node:path";
 import gulpEsbuild from "gulp-esbuild";
 
 // 清理构建目录
@@ -10,8 +9,12 @@ async function clean() {
 
 // 构建 preview-src
 function buildPreview() {
-  gulp.src('node_modules/@vscode/codicons/dist/codicon.css').pipe(gulp.dest('media/'));
-  gulp.src('node_modules/@vscode/codicons/dist/codicon.ttf').pipe(gulp.dest('media/'));
+  gulp
+    .src("node_modules/@vscode/codicons/dist/codicon.css")
+    .pipe(gulp.dest("media/"));
+  gulp
+    .src("node_modules/@vscode/codicons/dist/codicon.ttf")
+    .pipe(gulp.dest("media/"));
   return gulp
     .src("preview-src/**/*.ts")
     .pipe(
@@ -30,12 +33,14 @@ function buildPreview() {
 // 构建 src
 async function buildSrc() {
   return gulp
-    .src("src/**/*.ts")
+    .src("src/extension.ts")
     .pipe(
       gulpEsbuild({
         target: "es2022",
         format: "cjs",
         sourcemap: false,
+        bundle: true,
+        external: ["vscode"],
       })
     )
     .pipe(gulp.dest("out/"));
